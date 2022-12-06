2b80037bf49c7810b7c29ef64a262ad756e33a7e
Hemlata Malve <2000081411@hexaware.com>
2022-12-06 19:00:59 +0530

    First

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Cmsfinalapp'`, () => {
+  it(`should have as title 'claim-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Cmsfinalapp');
+    expect(app.title).toEqual('claim-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Cmsfinalapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('claim-app app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DisplayComponent } from './login/display/display.component';
+import { PpDisplayComponent } from './pp-display/pp-display.component';
+import { PpRegComponent } from './pp-register/pp-reg.component';
+import { PpShowComponent } from './pp-show/pp-show.component';
+// import { PpRegComponent } from './pp-register/pp-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    SignupComponent,
+    DisplayComponent,
+    PpDisplayComponent,
+    PpRegComponent,
+    PpShowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
